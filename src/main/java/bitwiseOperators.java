3a5348cad3f570bf51f2072f2016716ffe4755f1
vladikshkwok <vladikshwok@gmail.com>
2022-04-11 20:44:51 +0300

    added not a and not b (binary and decimal)

--- src/main/java/bitwiseOperators.java
+++ src/main/java/bitwiseOperators.java
@@ -4,10 +4,15 @@ public class bitwiseOperators {
         String aBinary = Integer.toBinaryString(a),
                 bBinary = Integer.toBinaryString(b);
 
-        System.out.println(aBinary);
-        System.out.println(bBinary);
+
+        System.out.println(a + " = " + aBinary);
+        System.out.println(b + " = " + bBinary);
         System.out.println(aBinary + " & " + bBinary + " = " + Integer.toBinaryString(a & b));
         System.out.println(aBinary + " | " + bBinary + " = " + Integer.toBinaryString(a | b));
         System.out.println(aBinary + " ^ " + bBinary + " = " + Integer.toBinaryString(a ^ b));
+        System.out.println("~" + aBinary + " = " + Integer.toBinaryString(~a));
+        System.out.println("~" + bBinary + " = " + Integer.toBinaryString(~b));
+        System.out.println("~" + a + " = " + ~a);
+        System.out.println("~" + b + " = " + ~b);
     }
 }
