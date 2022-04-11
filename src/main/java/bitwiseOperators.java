a95c6ad8559046032ad70f2b8b716b73b8579f13
vladikshkwok <vladikshwok@gmail.com>
2022-04-11 21:29:59 +0300

    bit shift

--- src/main/java/bitwiseOperators.java
+++ src/main/java/bitwiseOperators.java
@@ -1,17 +1,27 @@
 public class bitwiseOperators {
     public static void main(String[] args) {
-        int a = 7, b = 4;
+        int a = 9, b = 5, aShiftRight, bShiftRight, aShiftLeft, bShiftLeft;
+        aShiftRight = a >> 2;
+        bShiftRight = b >> 2;
+        aShiftLeft = a << 2;
+        bShiftLeft = b << 2;
         String aBinary = Integer.toBinaryString(a),
                 bBinary = Integer.toBinaryString(b),
                 notaBinary = Integer.toBinaryString(~a),
-                notbBinary = Integer.toBinaryString(~b);
+                notbBinary = Integer.toBinaryString(~b),
+                aShiftBinaryRight = Integer.toBinaryString(aShiftRight),
+                bShiftBinaryRight = Integer.toBinaryString(bShiftRight),
+                aShiftBinaryLeft = Integer.toBinaryString(aShiftLeft),
+                bShiftBinaryLeft = Integer.toBinaryString(bShiftLeft);
 
 
         System.out.println(a + " = " + aBinary);
         System.out.println(b + " = " + bBinary);
+
         System.out.println(aBinary + " & " + bBinary + " = " + Integer.toBinaryString(a & b));
         System.out.println(aBinary + " | " + bBinary + " = " + Integer.toBinaryString(a | b));
         System.out.println(aBinary + " ^ " + bBinary + " = " + Integer.toBinaryString(a ^ b));
+
         System.out.println("~" + aBinary + " = " + notaBinary);
         System.out.println("~" + bBinary + " = " + notbBinary);
         System.out.println("~" + a + " = " + ~a);
@@ -19,5 +29,15 @@ public class bitwiseOperators {
 //        NumberFormatException
 //        System.out.println(Integer.parseInt(notaBinary, 2));
 //        System.out.println(Integer.parseInt(notbBinary, 2));
+
+        System.out.println(a + " >> 2 = " + aShiftRight);
+        System.out.println(b + " >> 2 = " + bShiftRight);
+        System.out.println(aBinary + " >> 2 = " + aShiftBinaryRight);
+        System.out.println(bBinary + " >> 2 = " + bShiftBinaryRight);
+
+        System.out.println(a + " << 2 = " + aShiftLeft);
+        System.out.println(b + " << 2 = " + bShiftLeft);
+        System.out.println(aBinary + " << 2 = " + aShiftBinaryLeft);
+        System.out.println(bBinary + " << 2 = " + bShiftBinaryLeft);
     }
 }
