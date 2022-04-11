61abc6aeaf9c8e385f9d57962f62948d3c25ed49
vladikshkwok <vladikshwok@gmail.com>
2022-04-11 21:09:55 +0300

    Found strange exception

--- src/main/java/bitwiseOperators.java
+++ src/main/java/bitwiseOperators.java
@@ -2,7 +2,9 @@ public class bitwiseOperators {
     public static void main(String[] args) {
         int a = 7, b = 4;
         String aBinary = Integer.toBinaryString(a),
-                bBinary = Integer.toBinaryString(b);
+                bBinary = Integer.toBinaryString(b),
+                notaBinary = Integer.toBinaryString(~a),
+                notbBinary = Integer.toBinaryString(~b);
 
 
         System.out.println(a + " = " + aBinary);
@@ -10,9 +12,12 @@ public class bitwiseOperators {
         System.out.println(aBinary + " & " + bBinary + " = " + Integer.toBinaryString(a & b));
         System.out.println(aBinary + " | " + bBinary + " = " + Integer.toBinaryString(a | b));
         System.out.println(aBinary + " ^ " + bBinary + " = " + Integer.toBinaryString(a ^ b));
-        System.out.println("~" + aBinary + " = " + Integer.toBinaryString(~a));
-        System.out.println("~" + bBinary + " = " + Integer.toBinaryString(~b));
+        System.out.println("~" + aBinary + " = " + notaBinary);
+        System.out.println("~" + bBinary + " = " + notbBinary);
         System.out.println("~" + a + " = " + ~a);
         System.out.println("~" + b + " = " + ~b);
+//        NumberFormatException
+//        System.out.println(Integer.parseInt(notaBinary, 2));
+//        System.out.println(Integer.parseInt(notbBinary, 2));
     }
 }
